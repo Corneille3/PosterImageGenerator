f713f92d8d1c185c62fab1222977138101c0410c
JosimarKouam <105067242+Corneille3@users.noreply.github.com>
2026-01-16 15:51:17 -0500

    Update backend.tf with code

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "YOUR_STATE_BUCKET_NAME"
+    key            = "poster-image/dev/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
