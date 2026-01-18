5b559308ee80bc4ef64758806c072bb7828dcd7e
JosimarC <josycorneille@yahoo.fr>
2026-01-18 17:30:22 -0500

    added output to both dev and pro

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -5,17 +5,17 @@ terraform {
 module "poster_api" {
   source = "../../modules/poster_api"
 
-  env                 = "dev"
-  region              = "us-east-2"
-  bucket_name          = "myovieostermageenerator03"
+  env                = "dev"
+  region             = "us-east-2"
+  bucket_name         = "myovieostermageenerator03"
 
-  bedrock_region       = "us-west-2"
-  model_id             = "stability.sd3-5-large-v1:0"
+  bedrock_region      = "us-west-2"
+  model_id            = "stability.sd3-5-large-v1:0"
 
-  key_prefix           = "generated/dev/"
-  url_expires_seconds  = 3600
+  key_prefix          = "generated/dev/"
+  url_expires_seconds = 3600
 
-  lambda_src_dir       = "${path.module}/../../../lambda"
+  lambda_src_dir      = "${path.module}/../../../lambda"
 }
 
 output "api_base_url" {
@@ -24,4 +24,16 @@ output "api_base_url" {
 
 output "invoke_url" {
   value = module.poster_api.invoke_url
+}
+
+output "cognito_user_pool_id" {
+  value = module.poster_api.cognito_user_pool_id
+}
+
+output "cognito_app_client_id" {
+  value = module.poster_api.cognito_app_client_id
+}
+
+output "cognito_issuer" {
+  value = module.poster_api.cognito_issuer
 }
