7333d4a540d4cf6e7dfca44459b955a5eab30c1c
Josimar Kouam <josycorneille3@gmail.com>
2026-01-18 10:57:58 -0500

    added api_base_url output

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -5,20 +5,23 @@ terraform {
 module "poster_api" {
   source = "../../modules/poster_api"
 
-  env                = "dev"
-  region             = "us-east-2"
-  bucket_name         = "myovieostermageenerator03"
+  env                 = "dev"
+  region              = "us-east-2"
+  bucket_name          = "myovieostermageenerator03"
 
-  bedrock_region      = "us-west-2"
-  model_id            = "stability.sd3-5-large-v1:0"
+  bedrock_region       = "us-west-2"
+  model_id             = "stability.sd3-5-large-v1:0"
 
-  key_prefix          = "generated/dev/"
-  url_expires_seconds = 3600
+  key_prefix           = "generated/dev/"
+  url_expires_seconds  = 3600
 
-  lambda_src_dir      = "${path.module}/../../../lambda"
+  lambda_src_dir       = "${path.module}/../../../lambda"
+}
+
+output "api_base_url" {
+  value = module.poster_api.api_base_url
 }
 
 output "invoke_url" {
   value = module.poster_api.invoke_url
 }
-
