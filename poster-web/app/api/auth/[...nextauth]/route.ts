42828108d023c8c87c8115e010847fb74dec6901
JosimarC <josycorneille@yahoo.fr>
2026-01-20 19:41:44 -0500

    route.js to route.ts

---
+++ poster-web/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import CognitoProvider from "next-auth/providers/cognito";
+
+const handler = NextAuth({
+  providers: [
+    CognitoProvider({
+      clientId: process.env.COGNITO_CLIENT_ID!,
+      clientSecret: process.env.COGNITO_CLIENT_SECRET!,
+      issuer: process.env.COGNITO_ISSUER!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account?.id_token) {
+        token.idToken = account.id_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.idToken = token.idToken as string;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
